acbf4e1fe8c574f8d1608c15cb91f2429f9dc47f
samnags <sam.nagourney@gmail.com>
2017-05-10 18:40:26 -0700

    added detail table

--- client/src/components/member_detail.js
+++ client/src/components/member_detail.js
@@ -1,7 +1,35 @@
 import React from 'react'
+import Reactable from 'reactable'
+
+var Table = Reactable.Table;
 
 const MemberDetail = (props) => {
     const { attendances } = props
+    
+    let tableRows = null
+    
+    if(attendances.length > 0) {
+        tableRows = attendances.map((attendance) => {            
+          return (
+            { 
+                Date: attendance.meeting.date,
+                'In Session': attendance.meeting.in_session,
+                Attended: attendance.attended            
+            }
+          )
+      })
+    }
+    
+    return (
+        <Table
+            className={'table table-bordered table-responsive table-hover'}
+            data={tableRows}
+            // filterable={['Quantity', 'Measurment', 'Name', 'Category']}
+            // filterable={['Category']}
+            // noDataText="No matching records found."
+            // onClick={props.onClick}
+            />
+    )
 }
 
 module.exports = MemberDetail
